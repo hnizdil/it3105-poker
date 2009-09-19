db8562da172cb1329c7867d43b13457c0832c7b2
robert-braunschweig <robert-braunschweig@gmx.de>
2009-09-19 19:50:36 +0200

    implementation of GoodBotPlayer calcHandStrength looks good, but is not tested. getAction needs some adjusting and testing

--- src/GoodBotPlayer.java
+++ src/GoodBotPlayer.java
@@ -4,6 +4,7 @@ import java.util.Random;
 /**
  * Class for a better artificial Player with pre-flop simulation
  * and hand-strength calculation
+ * TODO testing
  * @author Robert Braunschweig
  *
  */
@@ -48,7 +49,7 @@ public class GoodBotPlayer extends Player{
 			ArrayList<Card> cards = new ArrayList<Card>(comCards);
 			cards.add(hole[0]);
 			cards.add(hole[1]);
-			float handStrength = calcHandStrength(cards);
+			float handStrength = calcHandStrength(cards,numberOfActive);
 			if(handStrength < 0.5)
 				act = Action.FOLD;
 			else if(handStrength < 0.65){
@@ -63,8 +64,53 @@ public class GoodBotPlayer extends Player{
 		return act;
 	}
 	
-	private float calcHandStrength(ArrayList<Card> cards){
+	/*generates the index tuples for all two hole cards out of the remaining deck
+	 * of size N
+	 *  
+	 */
+	private ArrayList<int[]> genIndexSet(int N){
+		ArrayList<int[]> res = new ArrayList<int[]>();
+		int[] tuple;
+		for(int i=0; i<N; i++)
+			for(int j=0; j<i; j++){
+				tuple = new int[2];
+				tuple[0] = i;
+				tuple[1] = j;
+				res.add(tuple);
+			}
+		return res;
+	}
+	
+	/*
+	 * calculates probability of win of the current Hand
+	 */
+	private float calcHandStrength(ArrayList<Card> comCards, int numberOfActive){
 		float res;
+		ArrayList<int[]> indexSet = genIndexSet(50-comCards.size());
+		ArrayList<Card> deck = Game.getInstance().getDeck();
+		Player p;
+		Card[] hole = new Card[2];	//hole for comparison
+		ArrayList<Player> players = new ArrayList<Player>();
+		Player[] winner;
+		int[] statistic = new int[]{0,0,0};	//win - loss - tie
+		players.add(this);	//the calling player is always part of the List
+		for(int[] pair: indexSet){
+			hole[0] = deck.get(pair[0]);
+			hole[1] = deck.get(pair[1]);
+			p = new GoodBotPlayer(hole);
+			players.add(p);
+			winner = Game.getInstance().getWinner(players);
+			if(winner.length > 1)	//tie
+				statistic[2]++;
+			else
+				if(winner[0].equals(this))	//win
+					statistic[0]++;
+				else						//loss
+					statistic[1]++;	
+		}
+		//equation from script (paper)
+		res = (statistic[0]+(statistic[2]/2))/(statistic[0]+statistic[1]+statistic[2]);
+		res = (float)Math.pow((double)res, (double)numberOfActive);
 		return res;
 	}
 
