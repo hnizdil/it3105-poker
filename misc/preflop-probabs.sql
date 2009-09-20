5666c7242d65780c26686b38c522acca151ad4f2
Jan Hnizdil <jan.hnizdil@dobryweb.cz>
2009-09-20 18:13:07 +0200

    Preflop table ready to use

--- misc/preflop-probabs.sql
+++ misc/preflop-probabs.sql
@@ -2,9 +2,10 @@ SELECT
 	`ec`.`card1`,
 	`ec`.`card2`,
 	`ec`.`suited`,
+	`r`.`players`,
 	ROUND(SUM(`r`.`wins`) / SUM(`r`.`rounds`), 9) AS `probab`
 
 FROM `preflop_ec` AS `ec`
 INNER JOIN `preflop_ec_runs` AS `r` ON `r`.`ec` = `ec`.`id`
 
-GROUP BY `ec`.`id`;
+GROUP BY `r`.`ec`, `r`.`players`;
